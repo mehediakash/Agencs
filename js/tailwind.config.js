97f9e746ab6074fb6a564bd420fd9a1f1ad37d6a
mehediakash <hacker.aklogic@gmail.com>
2022-09-15 14:59:39 +0600

    Initial commit

---
+++ js/tailwind.config.js
@@ -0,0 +1,24 @@
+tailwind.config = {
+  theme: {
+    extend: {
+      colors: {
+        primeryColor: "#6A4DF4",
+        "text-color": "#151515",
+        "pera-color": "#737373",
+      },
+      backgroundColor: {
+        "section-color": "#F8F6FE",
+      },
+      fontFamily: {
+        inter: "Inter",
+      },
+      padding: {
+        sgap: "150px",
+      },
+      maxWidth: {
+        container: "1320px",
+        margin: "0 auto",
+      },
+    },
+  },
+};
